d4a87fd26bebaed261f7058ff68d59b4f05ef5bc
Daniel <daniel.correa@veevart.com>
2023-10-17 07:29:05 -0500

    First commit CardViwe

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,15 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+       /* VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Helloooo")
         }
+        .padding()
+        .font(.largeTitle)
+        .foregroundColor(.red)
+        */
+        
+        HStack {
+            CardView()
+            CardView(isFaceUp: true)
+            CardView()
+        }
+        .foregroundColor(.green)
         .padding()
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12).strokeBorder(lineWidth: 2)
+                Text("🐸").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
